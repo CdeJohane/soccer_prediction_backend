c4e8378f1056b68fec4b9285685d4483464532e7
CdeJohane <j.r.t.johane@gmail.com>
2024-09-06 07:25:08 +0200

    Updated App.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from controllers.matches import match_bp
+from controllers.players import player_bp
+from controllers.predict import predict_bp
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+CORS(app)
+app.config.from_object("config.DevelopmentConfig")
+app.register_blueprint(match_bp)
+app.register_blueprint(player_bp)
+app.register_blueprint(predict_bp)
+if __name__ == "__main__":
+    app.run(debug=True)
